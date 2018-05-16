3fcdf707b741798e27dbb20b58056688359cd73e
Shiva (work) <m.shevlyakov@gmail.com>
2018-05-16 15:14:42 +0300

    Connection to Mosquitto DONE!

--- components/MqttText.js
+++ components/MqttText.js
@@ -3,57 +3,71 @@ import init from 'react_native_mqtt';
 import { AsyncStorage, StyleSheet, Text, View } from 'react-native';
 
 init({
-  size: 10000,
-  storageBackend: AsyncStorage,
-  defaultExpires: 1000 * 3600 * 24,
-  enableCache: true,
-  sync: {},
+    size: 10000,
+    storageBackend: AsyncStorage,
+    defaultExpires: 1000 * 3600 * 24,
+    enableCache: true,
+    sync: {},
 });
 
 export default class MqttText extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    const client = new Paho.MQTT.Client('10.10.10.215', 8083, 'unique_client_name');
-    client.onConnectionLost = this.onConnectionLost;
-    client.onMessageArrived = this.onMessageArrived;
-    client.connect({ onSuccess: this.onConnect, useSSL: false });
+        const client = new Paho.MQTT.Client('10.10.10.215', 8083, 'unique_client_name');
+        client.onConnectionLost = this.onConnectionLost;
+        client.onMessageArrived = this.onMessageArrived;
+        client.connect({ onSuccess: this.onConnect, useSSL: false });
 
-    this.state = {
-      text: ['---'],
-      client,
-    };
-  }
-
-  pushText = entry => {
-    const { text } = this.state;
-    this.setState({ text: [...text, entry] });
-  };
-
-  onConnect = () => {
-    const { client } = this.state;
-    client.subscribe('gateway/b827ebfffe688fd7/stats');
-    this.pushText(`connected`);
-  };
-
-  onConnectionLost = responseObject => {
-    if (responseObject.errorCode !== 0) {
-      this.pushText(`connection lost: ${responseObject.errorMessage}`);
+        this.state = {
+            text: '',
+            payload: {},
+            client,
+        };
     }
-  };
 
-  onMessageArrived = message => {
-    this.pushText(`new message: ${message.payloadString}`);
-  };
+    pushText = entry => {
+        const { text } = this.state;
+        this.setState({ text: [...text, entry] });
+    };
 
-  render() {
-    const { text } = this.state;
+    onConnect = () => {
+        const { client } = this.state;
+        client.subscribe('gateway/b827ebfffe688fd7/stats');
+        this.pushText(`connected`);
+    };
 
-    return (
-      <View>
-        <Text>{JSON.stringify(this.state.client)}</Text>
-        {text.map(entry => <Text key={entry} >{entry}</Text>)}
-      </View>
-    );
-  }
+    onConnectionLost = responseObject => {
+        if (responseObject.errorCode !== 0) {
+            this.pushText(`connection lost: ${responseObject.errorMessage}`);
+        }
+    };
+
+    onMessageArrived = message => {
+        this.setState({ payload: JSON.parse(message.payloadString), text:  message.payloadString});
+    };
+
+    render() {
+        const { text } = this.state;
+        const {
+            mac,
+            time,
+            rxPacketsReceived,
+            rxPacketsReceivedOK,
+            txPacketsReceived,
+            txPacketsEmitted
+        } = this.state.payload;
+
+        return (
+            <View>
+                <Text>Gateway Id: {mac}</Text>
+                <Text>Time: {time}</Text>
+                <Text>rxPacketsReceived: {rxPacketsReceived}</Text>
+                <Text>rxPacketsReceivedOK: {rxPacketsReceivedOK}</Text>
+                <Text>txPacketsReceived: {txPacketsReceived}</Text>
+                <Text>txPacketsEmitted: {txPacketsEmitted}</Text>
+                <Text>Last message: {this.state.text}</Text>
+            </View>
+        );
+    }
 }
